40229cc58f390d2fc16cf04806cec51d89fbca00
sandrix <sandragarciabescos@hotmail.es>
2018-04-18 10:36:48 +0200

    general structure

---
+++ gpsdemo4.m
@@ -0,0 +1,47 @@
+function gpsdemo4
+% Summary: 
+% 
+% Matlab Herrler
+%-------------------------------------------------------------------------
+% Sandra García Bescós
+% Matrikelnummer: 4869045
+%-------------------------------------------------------------------------
+%
+% function syntax:
+% function gpsdemo4
+% input parameter:  i             = 
+% 
+% output parameter: 
+%                   0 = 
+% variables:    
+%               [*,4] : importSeries 
+%               [*,1] : recSignal
+%                       correlation
+%                       pseudoT
+%               [2,4] : SpeakerPos
+%                       position
+%                       t
+%                       
+%% Perform function
+%% Import series
+
+
+%% Record signal
+
+
+%% calculate correlation and find max
+
+
+%% gps correction
+
+
+%% plots and position on map
+
+
+
+
+
+
+
+
+end
